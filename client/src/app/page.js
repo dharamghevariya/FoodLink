d22c441228f2d03b0b6a85dbb3b1367c186f9a43
dharamghevariya <dmghevariya27@gmail.com>
2025-03-21 19:46:01 -0400

    shadcn ready to use, added the navbar

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,7 +1,10 @@
+import Navbar from "@/components/navbar/navbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <></>
+    <>
+      <Navbar />
+    </>
   );
 }
